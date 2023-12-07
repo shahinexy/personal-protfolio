70996b768fe73048618c3aa19c43651d3be0b93e
shahinexy <shahinexy@gmail.com>
2023-12-07 14:46:45 +0600

    add all file

---
+++ assets/js/script.js
@@ -0,0 +1,7 @@
+// ====== typing animetion ======
+const typed = new Typed('.input', {
+  strings: ['Web Devoloper.', 'Web Designer.', 'Freelancer.'],
+  typeSpeed: 80,
+  backSpeed: 50,
+  loop:true
+});
